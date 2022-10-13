a4a8aa2e0dd55071f6d237cb9524f5f540c9bd1e
kostyamed <kostyamed0003@gmail.com>
2022-10-13 12:09:16 +0300

    Rewrited data storage and added adding rows

--- db.py
+++ db.py
@@ -1,15 +1,28 @@
-import rows as row
+import pandas as pd
+import copy
+
 
 class table:
     name = ""
-    columns = []
     column_params = []
-    rows = []
+    columns = []
+    data = pd.DataFrame()
     
     def __init__(self, table_name: str, table_columns: list, table_params: list) -> None:
         self.name = table_name
-        self.columns = table_columns
         self.column_params = table_params
-        
-    def add_row(self, data: list) -> None:
-        self.rows.append(row.row(data))
+        self.columns = table_columns
+        df1 = {}
+        for i in table_columns:
+            df1[i] = []
+        df = pd.DataFrame(df1)
+        self.data = df
+    
+    def add_row(self, row: list) -> None:
+        old_data = copy.deepcopy(self.data)
+        addition_data1 = {}
+        for i in range(len(self.columns)):
+            addition_data1[self.columns[i]] = [row[i]]
+        addition_data = pd.DataFrame(addition_data1)
+        new_data = pd.concat([old_data, addition_data], ignore_index=True)
+        self.data = new_data
